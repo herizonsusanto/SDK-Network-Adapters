bfc0ef6783fb4587c355589a4b3f0575de1da21a
Thomas So <thomas.m.so33@gmail.com>
2018-02-28 17:02:30 -0800

    Bump MoPub adapter versions -> MoPub-2.1.1

--- MoPub/iOS/AppLovinBannerCustomEvent.m
+++ MoPub/iOS/AppLovinBannerCustomEvent.m
@@ -63,7 +63,7 @@ static NSMutableDictionary<NSString *, ALAdView *> *ALGlobalAdViews;
     if ( adSize )
     {
         self.sdk = [self SDKFromCustomEventInfo: info];
-        [self.sdk setPluginVersion: @"MoPub-2.1.0"];
+        [self.sdk setPluginVersion: @"MoPub-2.1.1"];
         
         // Zones support is available on AppLovin SDK 4.5.0 and higher
         NSString *zoneIdentifier = info[@"zone_id"];
--- MoPub/iOS/AppLovinNativeCustomEvent.m
+++ MoPub/iOS/AppLovinNativeCustomEvent.m
@@ -47,7 +47,7 @@ static NSString *const kALMoPubMediationErrorDomain = @"com.applovin.sdk.mediati
 {
     [[self class] log: @"Requesting AppLovin native ad with info: %@", info];
     
-    [[ALSdk shared] setPluginVersion: @"MoPub-2.1.0"];
+    [[ALSdk shared] setPluginVersion: @"MoPub-2.1.1"];
     
     ALNativeAdService *nativeAdService = [ALSdk shared].nativeAdService;
     [nativeAdService loadNativeAdGroupOfCount: 1 andNotify: self];
