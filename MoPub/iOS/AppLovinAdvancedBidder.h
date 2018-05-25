4fafa84d3811f66ee91e099eac99736dff4c7d7f
Thomas So <thomas.m.so33@gmail.com>
2018-05-24 19:45:50 -0700

    Add initial AppLovin advanced bidder class

---
+++ MoPub/iOS/AppLovinAdvancedBidder.h
@@ -0,0 +1,21 @@
+//
+//  AppLovinAdvancedBidder.h
+//  SDK Network Adapters Test App
+//
+//  Created by Thomas So on 5/22/18.
+//  Copyright © 2018 AppLovin Corp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#if __has_include(<MoPub/MoPub.h>)
+    #import <MoPub/MoPub.h>
+#elif __has_include(<MoPubSDKFramework/MoPub.h>)
+    #import <MoPubSDKFramework/MoPub.h>
+#else
+    #import "MPAdvancedBidder.h"
+#endif
+
+@interface AppLovinAdvancedBidder : NSObject<MPAdvancedBidder>
+
+@end
---
+++ MoPub/iOS/AppLovinAdvancedBidder.m
@@ -0,0 +1,23 @@
+//
+//  AppLovinAdvancedBidder.m
+//  SDK Network Adapters Test App
+//
+//  Created by Thomas So on 5/22/18.
+//  Copyright © 2018 AppLovin Corp. All rights reserved.
+//
+
+#import "AppLovinAdvancedBidder.h"
+
+@implementation AppLovinAdvancedBidder
+
+- (NSString *)creativeNetworkName
+{
+    return @"applovin";
+}
+
+- (NSString *)token
+{
+    return @"";
+}
+
+@end
