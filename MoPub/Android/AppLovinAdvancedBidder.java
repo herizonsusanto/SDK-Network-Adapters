68ea934494ad41851a2ed7d120f1fb6d77be727f
Thomas So <thomas.m.so33@gmail.com>
2018-05-30 20:39:20 -0700

    Check in AppLovinAdvancedBidder

---
+++ MoPub/Android/AppLovinAdvancedBidder.java
@@ -0,0 +1,24 @@
+package MoPub.Android;
+
+import android.content.Context;
+
+import com.mopub.common.MoPubAdvancedBidder;
+
+/**
+ * Include this class to use advanced bidding from AppLovin.
+ */
+public class AppLovinAdvancedBidder
+        implements MoPubAdvancedBidder
+{
+    @Override
+    public String getCreativeNetworkName()
+    {
+        return "applovin";
+    }
+
+    @Override
+    public String getToken(final Context context)
+    {
+        return null;
+    }
+}
